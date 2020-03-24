08fee1cf697c81b9736d748f3355ef2607e76e4a
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-03-24 12:51:03 -0400

    Created Seller profile
    Created seller profile using one to one link with built in user https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone

--- User/models.py
+++ User/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.TextField(max_length=30, blank=True)
+    address = models.CharField(max_length=400, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_seller(sender, instance, created, **kwargs):
+    if created:
+        Seller.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_seller(sender, instance, **kwargs):
+    instance.seller.save()
 
 # Create your models here.
