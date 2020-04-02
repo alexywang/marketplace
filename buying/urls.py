1d5f4cc2578c9d24492603c006064807330debe2
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-04-01 20:16:14 -0400

    Created index page in item and added item category

---
+++ buying/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path 
+import buying.views
+
+urlpatterns = []
+               
+               
--- items/models.py
+++ items/models.py
@@ -12,5 +12,10 @@ class Item(models.Model):
     description = models.TextField(max_length=500, blank=True)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     quantity = models.IntegerField(default=1)
-    sold = models.BooleanField(default=False)
+    sold = models.BooleanField(default=False) #do we need this? or can we update once its attached to an order 
+    #we should have an image field! 
+class ItemCategories(models.Model):
+    item_id= models.ForeignKey(Item,on_delete=models.CASCADE)
+    category_name= models.ForeignKey(Category,on_delete=models.CASCADE)
+
 
