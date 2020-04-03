89a003ab27fee827f20b7c5192a712cdd202c31e
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-04-02 23:59:22 -0400

    Added links

--- user/models.py
+++ user/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.TextField(max_length=15, blank=True)
-    address = models.CharField(max_length=400, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(max_length=400, blank=True)
 
