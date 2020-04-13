81d6d7ec0f283f208481a424a940cef6d084f81d
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-04-13 19:16:38 -0400

    Added a delete item (need to be careful with this)

--- items/urls.py
+++ items/urls.py
@@ -5,6 +5,8 @@ import items.views
 #what naming convention do we want to use 
 urlpatterns = [path('add',items.views.add_item, name="add_item"),
                path('myItems',items.views.my_items,name="my_items"),
-               path('edit/<int:pk>',items.views.edit_item,name="edit_item"),]
+               path('edit/<int:pk>',items.views.edit_item,name="edit_item"),
+               path('delete/<int:pk>',items.views.delete_item,name="delete_item"),
+            ]
                
                
