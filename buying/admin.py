15e744bb1eb9c03a77cf0a35685e1e70c33046f0
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-04-12 21:20:46 -0400

    Changed order model

--- buying/admin.py
+++ buying/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from buying.models import *
 
 admin.site.register(Order)
-admin.site.register(UserCartItem)
+
 
 # Register your models here.
--- buying/models.py
+++ buying/models.py
@@ -4,10 +4,7 @@ import items.models
 
 class Order(models.Model):
     order_id = models.AutoField(primary_key=True)
-    buyer  = models.ForeignKey(user.models.UserProfile, on_delete=models.SET_NULL, null=True) # Removed cascading so previous orders will still show if the buyer gets deleted
-    item = models.ForeignKey(items.models.Item, on_delete=models.SET_NULL, null=True)
+    buyer  = models.ForeignKey(user.models.UserProfile, on_delete=models.SET_NULL, null=True) 
+    items = models.ManyToManyField(items.models.Item)
     date = models.DateField()
 
-class UserCartItem(models.Model):
-    user = models.ForeignKey(user.models.UserProfile, on_delete=models.CASCADE)
-    item = models.ForeignKey(items.models.Item, on_delete=models.CASCADE)
