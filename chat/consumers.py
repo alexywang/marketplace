02406de8ca59269eafe968d64ccae29cceb84d78
Aidan Weber-Concannon <aidanwc98@gmail.com>
2020-04-13 22:30:11 -0400

    Added chat- will refine later(instructions below)
    brew install redis
    brew install openssl
    *use the env to run the project
    pip install channels
    pip install channels_redis

---
+++ chat/consumers.py
@@ -0,0 +1,67 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+from channels.auth import get_user, logout
+from django.contrib.auth.models import User
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        user = self.scope['user']
+        if user.is_authenticated:
+          async_to_sync(self.channel_layer.group_add)(
+              user.username,
+              self.channel_name
+          )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        user = self.scope['user']
+        
+        if user.is_authenticated:
+            message = user.username + ': ' + message
+        else:
+            message = 'Anonymous: ' + message
+        
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+        
+
+    # Receive message from room group
+    def chat_message(self, event):
+        message = event['message']
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+    
+    # Receive message from username group
+    def logout_message(self, event):
+        self.send(text_data=json.dumps({
+            'message': event['message']
+        }))
+        self.close()
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('buying/',include('buying.urls')),
     path('items/',include('items.urls')),
     path('admin/', admin.site.urls),
+    path('chat/',include('chat.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #this is only for development
